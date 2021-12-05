b1ff3699644fd7e12e08d80ec37d481de909fb5e
David Fournet <46575212+Dac56@users.noreply.github.com>
2021-12-05 15:33:06 -0500

    Updated files: PrimitiveManager.py - copied in methods from pypot library and beginning work on refactoring them / reworking them to be non-dependant on pypot code
    Created files:
    robot.py - to be used for robot layout set up and instantiation

---
+++ KoalbyHumanoid/robot.py
@@ -0,0 +1,8 @@
+"""
+To be used for robot instantiation.
+Possible functionalities:
+    - call robot URDF body layout file and call kinematic and IK methods
+    - set up config file and motor groupings
+    - handle sensor layout set up
+    -
+"""
