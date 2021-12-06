d1719252b18b77f2ad53471e3428fff50be22742
Anthony <agalgano@wpi.edu>
2021-12-05 21:25:02 -0500

    SerialArduino class push

--- ArduinoSerial.py
+++ ArduinoSerial.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import serial
 
+'''
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
@@ -9,3 +10,27 @@ if __name__ == '__main__':
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
+'''
+
+class ArduinoSerial:
+
+    def __init__(self):
+
+
+    def send_command(self, command):
+        ser = self.initialze_buffer()
+        ser.write(b"Hello from Raspberry Pi!\n")
+
+    def read_command(self):
+        ser = self.initialze_buffer()
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+
+    def initialze_buffer(self):
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser.reset_input_buffer()
+        return ser
+
+
+
+
