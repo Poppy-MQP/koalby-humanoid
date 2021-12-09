0c515630039272c25fb0fa6bc8f130a79f4b76c5
R Ian Beazley <raymondibeazley@gmail.com>
2021-12-08 22:15:09 -0500

    Adding in code for future tests as well as figuring out what we need to add next for it to work

--- KoalbyHumanoid/robot.py
+++ KoalbyHumanoid/robot.py
@@ -7,12 +7,25 @@ Possible functionalities:
     - handle full robot-wide commands such as "shutdown"
 """
 import ArduinoSerial
+from KoalbyHumanoid.motor import Motor
+from Kinematics.CopiedIK import IKChain
 
 
 class Robot(object):
 
     def __init__(self):
-        pass
+        self.r_shoulder_x = Motor(0x01, [0, 100])  # This is made up. Change later
+        self.motors = list()
+        self.l_arm = list()
+        self.r_arm = list()
+        self.torso = list()
+
+        # Change these later
+        self.l_arm_chain = IKChain.from_poppy_creature(self, motors=self.torso + self.l_arm, passiv=self.torso,
+                                                       tip=[0, 0.18, 0])
+        self.r_arm_chain = IKChain.from_poppy_creature(self, motors=self.torso + self.r_arm, passiv=self.torso,
+                                                       tip=[0, 0.18, 0], reversed_motors=[self.r_shoulder_x])
+
 
     def shutdown(self):
         """sends command to the arduino to shutdown all motors on the entire robot and turn their LEDs red"""
--- Primitives/TestSuite.py
+++ Primitives/TestSuite.py
@@ -1,4 +1,6 @@
+import time
 from KoalbyHumanoid.motor import Motor
+from KoalbyHumanoid.robot import Robot
 
 """A simple test suite to check pi -> arduino communication and motor control"""
 
@@ -7,3 +9,38 @@ motor = Motor(0x01, -140)
 print(motor.getPosition())
 motor.setPositionPos(-90)
 print(motor.getPosition())
+
+
+# Ian Code. Temporary putting here
+def arm_follow_test():
+    koalby = Robot()
+    for m in koalby.motors:
+        m.goto_position(0, 2)
+
+    # Left arm is compliant, right arm is active
+    for m in koalby.l_arm:
+        m.compliant = False
+
+    for m in koalby.r_arm:
+        m.compliant = False
+
+    # The torso itself must not be compliant
+    for m in koalby.torso:
+        m.compliant = False
+
+    target_delta = 3
+    try:
+        while True:
+            follow_hand(koalby, target_delta)
+            time.sleep(10)
+
+    # Close properly the object when finished
+    except KeyboardInterrupt:
+        koalby.close()
+
+
+def follow_hand(koalby, delta):
+    """Tell the right hand to follow the left hand"""
+    right_arm_position = koalby.l_arm_chain.end_effector + delta
+    koalby.r_arm_chain.goto(right_arm_position, 0.5, wait=True)
+
