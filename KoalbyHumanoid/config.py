945e521cb25536b2a52d29f81918762f13e608db
agalgano08 <anthony.galgano@gmail.com>
2021-12-10 12:15:28 -0500

    Config file update

--- KoalbyHumanoid/config.py
+++ KoalbyHumanoid/config.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-from array import *
 '''
-
 Motor 1 - Herkulex, Right Forearm
 Motor 2 - Herkulex, Right Upper Arm
 Motor 3 - Herkulex, Right Arm Connector
@@ -15,9 +13,10 @@ Motor 7 - Herkulex, Right Shoulder
 Motor 11 - Herkulex, Torso Double Rotation Backside
 Motor 12 - Herkulex, Torso Double Rotation Frontside
 Motor 13 - Herkluex, Abdomen
+'''
 
- '''
 
+'''Array of all motors for Koalby.'''
 motors = [
     [0x01, [0,0], 'Herk', 'Right Forearm'],
     [0x02, [0,0], 'Herk', 'Right Upper Shoulder'],
@@ -27,7 +26,7 @@ motors = [
     [0x0B, [0,0], 'Herk', 'Left Forearm'],
     [0x0A, [0,0], 'Herk', 'Left Upper Shoulder'],
     [0x06, [0,0], 'Herk', 'Left Arm Connector'],
-    [0x07, 'Herk', 'Left Shoulder'],
+    [0x07, [0,0], 'Herk', 'Left Shoulder'],
 
     [0x11, [0,0], 'Herk', 'Torso Double Rotation Back'],
     [0x12, [0,0], 'Herk', 'Torso Double Rotation Front'],
@@ -35,11 +34,13 @@ motors = [
 
 ]
 
-
+''' Array of all motor groups for Koalby.'''
 motorGroups = [
     ['Right Arm', motors[0:4]],
     ['Left Arm', motors[4:8]],
     ['Chest', motors[8:11]],
 ]
+
+
 
 
