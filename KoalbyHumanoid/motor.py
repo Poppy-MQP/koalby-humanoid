443b719ddc1b5bf5ce75c69c4a66c72145e97385
David Fournet <46575212+Dac56@users.noreply.github.com>
2021-12-14 17:40:34 -0500

    Reworked ArduinoSerial.py and motor.py to get pi to arduino communications working.
    Updated TestSuite.py to try out different cases using the pi to arduino communications system

--- KoalbyHumanoid/motor.py
+++ KoalbyHumanoid/motor.py
@@ -1,5 +1,6 @@
 import ArduinoSerial
-#import config
+
+# import config
 
 """The Motor class hold all information for an abstract motor on the physical robot. It is used to interface with the
 arduino which directly controls the motors"""
@@ -10,29 +11,32 @@ class Motor(object):
     def __init__(self, _motorID, angleLimit):
         self.motorID = _motorID
         self.angle_limit = angleLimit
-        self.present_position = self.getPosition()
+        # self.present_position = self.getPosition()
+
+        self.arduino_serial = ArduinoSerial.ArduinoSerial()
+
+        self.arduino_serial.send_command('150')
 
     def getPosition(self):
         """reads the motor's current position from the arduino and returns the value in degrees"""
         idArr = [1, self.motorID]
-        ArduinoSerial.send_command(','.join(map(str,idArr)))
-        print("here")
-        currentPosition = ArduinoSerial.read_command()
+        self.arduino_serial.send_command(','.join(map(str, idArr)))
+        # print("get position")
+        currentPosition = self.arduino_serial.read_command()
         return currentPosition
 
     def setPositionPos(self, position):
         """sends a desired motor position to the arduino"""
         idPosArr = [2, self.motorID, position]
-
-        ArduinoSerial.send_command(','.join(map(str,idPosArr)))
-        print(ArduinoSerial.read_command())
+        self.arduino_serial.send_command(','.join(map(str, idPosArr)))
+        # print(ArduinoSerial.read_command())
 
     def setPositionTime(self, position, time):
         """sends a desired motor position to the arduino <to be executed in a set amount of time?>"""
         idPosTimeArr = [3, self.motorID, position, time]
-        ArduinoSerial.send_command(','.join(map(str, idPosTimeArr)))
+        self.arduino_serial.send_command(','.join(map(str, idPosTimeArr)))
 
     def torqueOnOff(self, toggle):
         """turns the torque of a motor on or off based on a 1 or 0 input and sends this to the arduino"""
         idBoolArr = [5, self.motorID, toggle]
-        ArduinoSerial.send_command(idBoolArr)
+        self.arduino_serial.send_command(idBoolArr)
--- Primitives/TestSuite.py
+++ Primitives/TestSuite.py
@@ -1,46 +1,55 @@
 import time
+
+import ArduinoSerial
 from KoalbyHumanoid.motor import Motor
 from KoalbyHumanoid.robot import Robot
 
 """A simple test suite to check pi -> arduino communication and motor control"""
 
-motor = Motor(0x0B, [-3, 140])
+'''
+NEXT STEPS:
+Move arduino_serial init from motor class to robot class.
+Pass arduino serial from robot into motor as param.
+Refactor command enums on arduino to avoid using 0 for init cmd and to allow for future additons to command list
+'''
 
+motor = Motor(0, [-3, 140])
+time.sleep(10)
 print(motor.getPosition())
-motor.setPositionPos(2)
+motor.setPositionPos(50)
 print(motor.getPosition())
 
 
-# Ian Code. Temporary putting here
-def arm_follow_test():
-    koalby = Robot()
-    for m in koalby.motors:
-        m.goto_position(0, 2)
-
-    # Left arm is compliant, right arm is active
-    for m in koalby.l_arm:
-        m.compliant = False
-
-    for m in koalby.r_arm:
-        m.compliant = False
-
-    # The torso itself must not be compliant
-    for m in koalby.torso:
-        m.compliant = False
-
-    target_delta = 3
-    try:
-        while True:
-            follow_hand(koalby, target_delta)
-            time.sleep(10)
-
-    # Close properly the object when finished
-    except KeyboardInterrupt:
-        koalby.close()
-
-
-def follow_hand(koalby, delta):
-    """Tell the right hand to follow the left hand"""
-    right_arm_position = koalby.l_arm_chain.end_effector + delta
-    koalby.r_arm_chain.goto(right_arm_position, 0.5, wait=True)
+# # Ian Code. Temporary putting here
+# def arm_follow_test():
+#     koalby = Robot()
+#     for m in koalby.motors:
+#         m.goto_position(0, 2)
+#
+#     # Left arm is compliant, right arm is active
+#     for m in koalby.l_arm:
+#         m.compliant = False
+#
+#     for m in koalby.r_arm:
+#         m.compliant = False
+#
+#     # The torso itself must not be compliant
+#     for m in koalby.torso:
+#         m.compliant = False
+#
+#     target_delta = 3
+#     try:
+#         while True:
+#             follow_hand(koalby, target_delta)
+#             time.sleep(10)
+#
+#     # Close properly the object when finished
+#     except KeyboardInterrupt:
+#         koalby.close()
+#
+#
+# def follow_hand(koalby, delta):
+#     """Tell the right hand to follow the left hand"""
+#     right_arm_position = koalby.l_arm_chain.end_effector + delta
+#     koalby.r_arm_chain.goto(right_arm_position, 0.5, wait=True)
 
