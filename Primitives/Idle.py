3f8da9163d81a55a3bd263e37bb2845a11b5120f
David Fournet <46575212+Dac56@users.noreply.github.com>
2022-02-18 16:56:20 -0500

    wrote possible idle primitive code in doc comment at end of idle being used for primitive testing.

--- Primitives/Idle.py
+++ Primitives/Idle.py
@@ -1,10 +1,8 @@
 """
-Dance Primitive
+Idle Primitive
 
-    DanceToBeat()
-        cause robot to move rythmically in response to a beats per minute (BPM) input
+Causes Koalby to move/sway slightly in an idle animation
 """
-import time
 
 from Primitives import KoalbyPrimitive
 
@@ -24,3 +22,24 @@ class Idle(KoalbyPrimitive.Primitive):
                                    1: 0,
                                    0: 0}
 
+
+'''
+import random
+from KoalbyHumanoid.config import motors
+from Primitives import KoalbyPrimitive
+
+class Idle(KoalbyPrimitive.Primitive):
+
+    def __init__(self, motors):
+        super().__init__()  # inheritance
+        self.motorPositionsDict = {}
+        self.isActive = True
+
+    def idleAnimation(self):
+        # Currently only moves the arms
+        self.motorPositionsDict = {}  # Clear the dictionary
+        for index in range(0, 8):  # Set depth to run in config file (0-4 is right arm motors)
+            motorID = motors[index][0]  # Get motor ID
+            motorPos = random.randrange(0, 15, 5)  # Generate random positons between 0 and 100
+            self.motorPositionsDict[motorID] = motorPos  # add position to dictionary
+'''
