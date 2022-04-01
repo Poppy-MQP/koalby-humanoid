864004335ecd55e3bab46d97c97bc05765af7775
David Fournet <46575212+Dac56@users.noreply.github.com>
2022-04-01 18:41:04 -0400

    fixed compliance during replay record
    NOTE: should make replay record based on motor groups

--- Primitives/ReplayPrimitive.py
+++ Primitives/ReplayPrimitive.py
@@ -39,15 +39,17 @@ class ReplayPrimitive(KoalbyPrimitive.Primitive):
         self.poseNum = int(input("Input number of poses desired:"))
         for m in self.Motors:
             m.compliantOnOff(1)  # sets all motors in the robot to be compliant for moving to poses
+            time.sleep(0.02)  # need delay for comm time
         for poseIndex in range(self.poseNum):  # for each pose from 0 to desired number of poses
             poseMotorPositionsDict = {}
-            self.continueSelect = int(input("Type 1 to record to next pose:"))  # wait for user to input "1" in console
+            self.continueSelect = int(input("Type 2 to record to next pose:"))  # wait for user to input "1" in console
             if self.continueSelect != 0:
+                time.sleep(0.1)  # delay to allow consistent reading of first motor in first pose
                 for m in self.Motors:  # for each motor in Motors list
                     poseMotorPositionsDict[m.motorID] = m.getPosition()  # add the motor ID as key and motor position as value
                 self.recordedPoses.append(poseMotorPositionsDict)  # add dictionary of current robot pose to list of recorded poses
             self.continueSelect = 0
-            time.sleep(0.01)
+            time.sleep(0.01)  # comms buffer delay
         # write dictionary of recorded poses to csv file
         motorIDHeaders = self.recordedPoses[0].keys()
         motionFile = open(str(input("Input saved file name:")), "w")  # request a filename
@@ -57,7 +59,7 @@ class ReplayPrimitive(KoalbyPrimitive.Primitive):
         motionFile.close()
         for m in self.Motors:
             m.compliantOnOff(0)  # set motors back to non-compliant for use elsewhere
-
+            time.sleep(0.02)  # need delay for comm time
 
     def change(self):
         if self.isActive:
